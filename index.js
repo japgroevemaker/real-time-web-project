7de7dcadb5beb3c87585834ff194335f47a8423e
japgroevemaker <joepgravemaker@gmail.com>
2018-04-17 14:33:54 +0200

    Re-writing with twitter-stream-api

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var bodyParser = require('body-parser')
 var app = express()
 var socket = require("socket.io")
 var Twit = require("twit")
-var TwitterStream = require('twitter-stream-api')
-var fs = require('fs');
+
 
 //static files
 
@@ -17,19 +16,6 @@ app.get("/", function(req, res) {
   res.render("index");
 })
 
-// var keys = {
-//     consumer_key : "roz8tFgK8w9elwL1K8dmpZSlt",
-//     consumer_secret : "mrIwaSIUK9LcIT5wYiujA2WNXWMJPvoupqHRVnf3TXMsPY7805",
-//     token : "741148952851034112-ETDOWGB8P1fkOLcf02Y28szagRBSYds",
-//     token_secret : "aB3fNVspjonDDXUVERns9ElHnPebJ9eQ3G8LeTTzRMLaU"
-// };
-//
-// var Twitter = new TwitterStream(keys, false);
-// Twitter.stream('statuses/filter', {
-//     track: 'javascript'
-// });
-//
-// Twitter.pipe(fs.createWriteStream('tweets.json'));
 
 var T = new Twit({
   consumer_key:         'roz8tFgK8w9elwL1K8dmpZSlt',
@@ -39,24 +25,35 @@ var T = new Twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 
-// var params = {
-//   q: "PSV since:2018-04-16",
-//   count: 1
-// }
+var params = {
+  track: 'Amsterdam',
+  language: 'nl'
+}
 
-var stream = T.stream('statuses/filter', { track: ['the'] })
+var stream = T.stream('statuses/filter', params);
 
 stream.on('tweet', function (tweet){
-  console.log(tweet);
-})
+  if (tweet.retweeted_status) {
+    console.log("");
+    console.log("Retweet");
+    console.log("");
+    if (tweet.retweeted_status.extended_tweet){
+      console.log(tweet.retweeted_status.extended_tweet.full_text);
+    } else {
 
-// function gotData(err, data, response) {
-//   console.log(data.statuses[0].text);
-// }
+      console.log(tweet.text);
+    }
+  }
 
-// T.get('search/tweets', params, gotData);
+  else {
+    console.log("");
+    console.log('Tweet');
+    console.log("");
+    console.log(tweet.text);
+  }
+});
+
 stream.on('tweet', function (tweet, stream, gotData){
-  // console.log(data.statuses[0].text);
 })
 
 var server = app.listen(3333, function() {
